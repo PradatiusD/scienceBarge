dee7bd8bd08398c23a3714171aa514b33800db33
PradatiusD <danielprada2012@gmail.com>
2015-03-25 14:48:58 -0400

    Added logic for mailchimp form

--- theme/front-page.php
+++ theme/front-page.php
@@ -1,17 +1,27 @@
 <?php
 
+function instafeed() {
+	wp_enqueue_script('instafeed', get_stylesheet_directory_uri(). '/js/instafeed.min.js', array(),'1.3.2', true);
+	wp_enqueue_script('instafeed_consumer', get_stylesheet_directory_uri(). '/js/instaconsumer.js', array('instafeed'), '1.0.0',true);
+	echo '<div id="instafeed"></div>';	
+}
+
 
 function do_custom_front_page () {
+
+
 	ob_start(); ?>
 
-	<?php // include('google-map.php'); ?>
-
-	<h1>Under Construction</h1>
+	<?php 
+		// include('google-map.php');
+		// instafeed();
+		echo do_shortcode('[gravityform id="1" title="true" description="true"]');
+	?>
 	<?php
 	echo ob_get_clean();
 }
 
-add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+add_filter('genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 remove_action('genesis_loop', 'genesis_do_loop');
 add_action('genesis_loop',    'do_custom_front_page');
 
---
+++ theme/js/instaconsumer.js
@@ -0,0 +1,11 @@
+
+// Documentation at http://instafeedjs.com/
+
+var userFeed = new Instafeed({
+  get: 'user',
+  userId: 1774511928,
+  accessToken: '1774511928.2c90c39.680a9751b646476e82df1b5719e02261',
+  resolution: 'standard_resolution'
+});
+
+userFeed.run();
